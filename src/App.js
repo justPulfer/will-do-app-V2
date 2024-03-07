bd13c6d0f416615de4e1c48a132df6babb58c99c
Pulfer Dadson <just_pulfer@Trudys-iMac.gateway.sasktel.net>
2024-03-06 16:44:41 -0800

    finished static design for desktop mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+const initialTasks = [
+	{
+		task: "Task 1",
+		completed: false,
+	},
+	{
+		task: "Task 2",
+		completed: false,
+	},
+	{
+		task: "Task 3",
+		completed: false,
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Main />
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<div className="header">
+			<p className="logo-text">
+				WILL-<span>DO</span>
+			</p>
+			<i class="fa-solid fa-arrow-right-from-bracket"></i>
+		</div>
+	);
+}
+
+function Main() {
+	return (
+		<div className="main">
+			<Progress />
+			<Form />
+			<TaskLists />
+		</div>
+	);
+}
+
+function Progress() {
+	return (
+		<div className="progress">
+			<p className="confirmation">
+				Todo Done<span className="encourage">Keep it up.</span>
+			</p>
+			<p className="num-completed">0/3</p>
+		</div>
+	);
+}
+
+function Form() {
+	return (
+		<form>
+			<input type="text" placeholder="What will you do?"></input>
+			<button>+</button>
+		</form>
+	);
+}
+
+function TaskLists() {
+	return (
+		<ul className="task-lists">
+			{initialTasks.map((taskItem) => (
+				<Task taskItem={taskItem} key={taskItem.task} />
+			))}
+		</ul>
+	);
+}
+
+function Task({ taskItem }) {
+	return (
+		<li className="task">
+			<div>
+				<span>
+					<i class="fa-regular fa-circle"></i>
+				</span>
+				{taskItem.task}
+			</div>
+
+			<span>
+				<i class="fa-regular fa-pen-to-square"></i>
+				<i class="fa-regular fa-trash-can"></i>
+			</span>
+		</li>
+	);
 }
 
 export default App;
